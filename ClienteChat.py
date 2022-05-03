addd1d63ca85bddd9a0a342e48ccd16905a59a4b
Belli <66693949+Ivan-LB@users.noreply.github.com>
2022-05-03 03:15:31 -0700

    Add files via upload

---
+++ ClienteChat.py
@@ -0,0 +1,36 @@
+from email import message
+import socket
+import threading
+import serial
+
+serArduino = serial.Serial('COM5', 9600)
+nickname='Receptor'
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(('187.250.77.133',5001))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'identificador':
+                client.send(nickname.encode('ascii'))
+            else:
+                if message == '0':
+                    serArduino.write(b'0')
+                else:
+                    serArduino.write(b'1')
+                print(message)
+        except:
+            print("Se ha desconectado")
+            client.close()
+            break
+def write():
+    while True:
+        message=f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+
+receive_thread=threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread=threading.Thread(target=write)
+write_thread.start()
---
+++ ServidorChat.py
@@ -0,0 +1,51 @@
+from http import client
+from operator import index
+import threading
+import socket
+
+host = '' #187.250.77.133
+port = 5001
+
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen()
+
+clients=[]
+nicknames=[]
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+def handle(client):
+    while True:
+        try:
+            message=client.recv(1024)
+            broadcast(message)
+        except:
+            index=clients.index(client)
+            clients.remove(client)
+            client.close()
+            nicknames=nicknames[index]
+            broadcast(f'{nicknames} se ha desconectado'.encode('ascii'))
+            nicknames.remove(nicknames)
+            break
+def receive():
+    while True:
+        client,address=server.accept()
+        print(f'Conectado con {str(address)}')
+
+        client.send('identificador'.encode('ascii'))
+        nickname=client.recv(1024).decode('ascii')
+        if nickname != 'Receptor':
+            nickname='Emisor'
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'El identificador del cliente es {nickname}')
+        broadcast(f'{nickname} se ha conectado'.encode('ascii'))
+        client.send('Conectado'.encode('ascii'))
+
+        thread=threading.Thread(target=handle,args=(client,))
+        thread.start()
+print('El servidor esta escuchando...')
+receive()
